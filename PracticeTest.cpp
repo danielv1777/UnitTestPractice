b6aabfff35ce66881f6e60bf75c8fef05dfdd052
daniel <danielv1777@yahoo.com>
2020-11-16 22:40:04 -0800

    updated tests

--- PracticeTest.cpp
+++ PracticeTest.cpp
@@ -18,5 +18,70 @@ TEST(PracticeTest, is_simple_palindrome)
 {
     Practice obj;
     bool actual = obj.isPalindrome("Not a palindrome");
+    ASSERT_FALSE(actual);
+}
+
+TEST(PracticeTest, is_simple_palindrome1)
+{
+    Practice obj;
+    bool actual = obj.isPalindrome("");
     ASSERT_TRUE(actual);
+}
+
+TEST(PracticeTest, is_simple_palindrome2)
+{
+    Practice obj;
+    bool actual = obj.isPalindrome(" ");
+    ASSERT_TRUE(actual);
+}
+
+TEST(PracticeTest, is_simple_palindrome3)
+{
+    Practice obj;
+    bool actual = obj.isPalindrome("123");
+    ASSERT_FALSE(actual);
+}
+
+TEST(PracticeTest, is_simple_palindrome4)
+{
+    Practice obj;
+    bool actual = obj.isPalindrome("111");
+    ASSERT_TRUE(actual);
+}
+
+TEST(PracticeTest, is_simple_palindrome5)
+{
+    Practice obj;
+    bool actual = obj.isPalindrome("Mom");
+    ASSERT_TRUE(actual);
+}
+
+TEST(PracticeTest, sortDescending)
+{
+    Practice obj;
+		int first = 0;
+		int second = 1;
+		int third = 3;
+    bool actual = obj.sortDescending(first, second, third);
+    ASSERT_TRUE(actual);
+}
+
+TEST(PracticeTest, sortDescending1)
+{
+    Practice obj;
+		int first = 3;
+		int second = 2;
+		int third = 1;
+    bool actual = obj.sortDescending(first, second, third);
+    ASSERT_TRUE(actual);
+}
+
+TEST(PracticeTest, sortDescending2)
+{
+    Practice obj;
+		int first = 0;
+		int second = 0;
+		int third = 0;
+    bool actual = obj.sortDescending(first, second, third);
+    ASSERT_FALSE(actual);
 }
